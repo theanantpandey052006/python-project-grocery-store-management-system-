08607c82eeebabd8cc97e4185f0a5682cbe7d3e9
theanantpandey052006 <theanantpandey052006@gmail.com>
2025-11-23 19:45:27 +0530

    inventory_data.py

--- inventory_data.py
+++ inventory_data.py
@@ -1,8 +1,6 @@
-# inventory_data.py
-
-# The core data structure to hold the inventory: {item_name: [quantity, price]}
 INVENTORY = {
     "apples": [150, 0.50],
     "bananas": [200, 0.30],
     "milk": [50, 2.75]
-}
+
+}# putting the input
